2dc2e2efba3f6f7a0ee3891ac37a42ecb0d6c53e
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2023-02-23 09:41:45 +0200

    redeploy

--- src/chakra/button.ts
+++ src/chakra/button.ts
@@ -1,6 +1,6 @@
 import type { ComponentStyleConfig } from "@chakra-ui/theme";
 
-//! this is how you can style your component globaly and anythis specific add it there like the hight wont be added here
+//! this is how you can style your component globally and anything specific add it there like the hight wont be added here
 
 //& Control space bar with typescript gives you a list of all of the properties that this interface is expecting
 export const Button: ComponentStyleConfig = {
